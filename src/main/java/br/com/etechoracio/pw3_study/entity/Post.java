a4062184cab4126b395d939d0392dadd6c2ee1b0
Julian_Paulo <juliandamfirst@gmail.com>
2024-08-20 11:07:21 -0300

    Criação do projeto E-study

---
+++ src/main/java/br/com/etechoracio/pw3_study/entity/Post.java
@@ -0,0 +1,14 @@
+package br.com.etechoracio.pw3_study.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+@Getter
+@Setter
+@Entity
+@Table(name = "")
+public class Post {
+
+}
